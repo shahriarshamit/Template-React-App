a7d2537e21707c501516d50f5b94e12961d3163a
shahriarshamit <samdevstudio@gmail.com>
2023-08-04 20:18:48 +0100

    Initial Commit

---
+++ src/component/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./App.css";
+
+const App = () => {
+    return (<div className='app-root'>
+        <h1 className='heading__one'>React application!!!</h1>
+    </div>);
+};
+
+export default App;
